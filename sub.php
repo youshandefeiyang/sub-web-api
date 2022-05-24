54041fc919d7a8bd1c0f4cc4ae8d4c1f3d7cf763
肥羊 <yunspeedtest@gmail.com>
2022-05-24 21:40:00 +0800

    Add files via upload

---
+++ sub.php
@@ -0,0 +1,29 @@
+<?php
+header('Content-Type: application/json');
+$userText = $_POST['config'];
+function mk_dir()
+{
+    $dir = date('md/H/i', time());
+    if (is_dir('./' . $dir)) {
+        return $dir;
+    } else {
+        mkdir('./' . $dir, 0777, true);
+        return $dir;
+    }
+}
+function randName()
+{
+    $str = 'abcdefghijkmnpqrstwxyz23456789';
+    return substr(str_shuffle($str), 0, 6);
+}
+$path = '/' . mk_dir() . '/' . randName() . '.' . 'ini';
+function writeText($str, $fileName)
+{
+    $userFile = fopen($fileName, "w+");
+    fwrite($userFile, $str);
+    fclose($userFile);
+}
+writeText($userText, "./" . $path);
+$arr = array('code' => 0,'msg'=>"success" ,'data' => "https://subapi.v1.mk$path");
+echo json_encode($arr, 320);
+?>
