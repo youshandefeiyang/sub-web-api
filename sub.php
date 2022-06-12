4404f44f381eb574d32740ab80be564e21dc1a5a
肥羊 <yunspeedtest@gmail.com>
2022-06-12 18:03:32 +0800

    解决后端中文乱码问题（同步修正前端）！

--- sub.php
+++ sub.php
@@ -1,6 +1,6 @@
 <?php
 header('Content-Type: application/json');
-$userText = $_POST['config'] ?? null;
+$userText = urldecode($_POST['config']) ?? null;
 if (empty($userText)) {
     $arr = array('msg' => "failed", 'data' => "empty value");
     echo json_encode($arr, 320);
