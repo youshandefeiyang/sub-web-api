f2e88e0fb3fdd25785bb7577fc691bb9f9f304fc
肥羊 <youshandefeiyang@gmail.com>
2022-06-28 19:34:21 +0800

    Add files via upload

--- redirect.php
+++ redirect.php
@@ -1,12 +1,20 @@
 <?php
+
+namespace subconverter;
 $token = urldecode($_GET['token']) ?? null;
 if (empty($token)) {
     $arr = array('msg' => "failed", 'data' => "empty value");
     echo json_encode($arr, 320);
     exit();
 } else {
+    require __DIR__ . '/config/connect.php';
+    $sql = 'SELECT `jslist` FROM `mdfive` WHERE `inilist` = ?';
+    $stmt = $db->prepare($sql);
+    $inilist = $token;
+    $stmt->execute([$inilist]);
+    $jsname = $stmt->fetch()['jslist'];
     $reg = "/profiles\/(.)+.js/i";
-    $replacement = "profiles/script/$token.js";
+    $replacement = "profiles/script/$jsname.js";
     $pref = "../pref.toml";
     $newpref = preg_replace($reg, $replacement, file_get_contents($pref));
     file_put_contents($pref, $newpref);
