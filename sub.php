ed8a331c201dc30b8ae3f3ceedc52cad5b75b6b5
肥羊 <youshandefeiyang@gmail.com>
2022-07-01 12:26:21 +0800

    Add files via upload

--- sub.php
+++ sub.php
@@ -6,12 +6,13 @@ require __DIR__ . '/config/common.php';
 use config\common;
 
 header('Content-Type: application/json');
-$userText = urldecode($_POST['config']) ?? null;
-if (empty($userText)) {
+$param = $_POST['config'] ?? null;
+if (empty($param)) {
     $arr = array('msg' => "failed", 'data' => "empty value");
     echo json_encode($arr, 320);
     exit();
 } else {
+    $userText = urldecode($_POST['config']);
     $remote = new Commonfunction;
     $path = '/' . $remote->mk_dir('remoteconfig/' . date('Y/m/md', time())) . '/' . md5($userText) . '.' . 'ini';
     file_put_contents(".$path", $userText);
